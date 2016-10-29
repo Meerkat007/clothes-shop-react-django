a95fbfa21b0e2cdbbd524df99534fbc222e87a1b
Yuan <yuanji.yj@gmail.com>
2016-10-29 17:32:40 -0400

    some updates

---
+++ myproject/mysite/settings/dev.py
@@ -0,0 +1,20 @@
+from mysite.settings.base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+
+
