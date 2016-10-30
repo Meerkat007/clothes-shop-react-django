2f08dfb80cf742a054fe116bfd34c6aace4af4cc
Yuan <yuanji.yj@gmail.com>
2016-10-29 20:06:24 -0400

    new apps

--- myproject/mysite/settings/dev.py
+++ myproject/mysite/settings/dev.py
@@ -4,7 +4,7 @@ from mysite.settings.base import *
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+SECRET_KEY = 'qes@hlp9%+(l_)_d45!l6*)l(y%x8qj6g60ydealq7_1$8s89f'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
